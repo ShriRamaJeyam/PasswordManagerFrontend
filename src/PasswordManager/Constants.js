df69f130af6e8dd030f03c8a96f5b1a3121e16ce
Niranjan Kumar K S <niranjankumarks@outlook.com>
2025-03-23 13:47:32 +0530

    next

--- src/PasswordManager/Constants.js
+++ src/PasswordManager/Constants.js
@@ -1,5 +1,5 @@
 const constants = {
-    apiHome : 'http://localhost:3000/api/password-manager',
+    apiHome : 'https://vercel-niranjan-kumar-k-s-s-projects.vercel.app/password-manager',
     dbUserAuthHashMaker : 'ShriRamaChandra1008ShriRamaBadra'
 };
 
